e18e6fe74ff14346c963354eba347c4f0611c5ee
Langchao Wu <langchaowu@yahoo.com>
2022-05-03 17:58:29 -0400

    add icons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faUserPen,faHouse, faArrowRightFromBracket, faCartShopping} from '@fortawesome/free-solid-svg-icons'
+
+
+
 
 function Header() {
   return (
@@ -6,12 +11,12 @@ function Header() {
         <h1>GameShop</h1>
 
         <div className='icons'>
-            <i class="fa-solid fa-user-pen"></i>
-            <i class="fa-solid fa-house"></i>
-            <i class="fa-solid fa-user"></i>
-            <i class="fa-solid fa-arrow-right-from-bracket"></i>
-            <i class="fa-solid fa-cart-shopping"></i>
-
+            <FontAwesomeIcon className='fa-solid' icon={faUserPen}/>
+            <FontAwesomeIcon className='fa-solid' icon={faHouse} />
+            <FontAwesomeIcon className='fa-solid' icon={faUser} />
+            <FontAwesomeIcon className='fa-solid' icon={faArrowRightFromBracket} />
+            <FontAwesomeIcon className='fa-solid' icon={faCartShopping} />
+           
 
         </div>
 
