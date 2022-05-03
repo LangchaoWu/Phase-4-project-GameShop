10ee095ae94c90739519f1a07d92e8dd4d8b99e2
Langchao Wu <langchaowu@yahoo.com>
2022-05-03 17:00:18 -0400

    create Header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav>
+        <h1>GameShop</h1>
+
+        <div className='icons'>
+            <i class="fa-solid fa-user-pen"></i>
+            <i class="fa-solid fa-house"></i>
+            <i class="fa-solid fa-user"></i>
+            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            <i class="fa-solid fa-cart-shopping"></i>
+
+
+        </div>
+
+
+    </nav>
+  )
+}
+
+export default Header
